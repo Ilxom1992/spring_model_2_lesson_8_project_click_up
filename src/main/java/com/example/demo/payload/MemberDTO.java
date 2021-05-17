e89c24cc9e4b5603ae2f049db791aab52e9912da
ILXOM <ilxom.xojamurodov@gmail.com>
2021-05-16 23:30:24 -0700

    commit_17

--- src/main/java/com/example/demo/payload/MemberDTO.java
+++ src/main/java/com/example/demo/payload/MemberDTO.java
@@ -1,6 +1,7 @@
 package com.example.demo.payload;
 
 import com.example.demo.entity.enums.AddType;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Data;
 
 
@@ -8,6 +9,7 @@ import java.sql.Timestamp;
 import java.util.UUID;
 
 @Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class MemberDTO {
     private UUID id;
 
--- src/main/java/com/example/demo/payload/WorkspaceDTO.java
+++ src/main/java/com/example/demo/payload/WorkspaceDTO.java
@@ -1,11 +1,13 @@
 package com.example.demo.payload;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Data;
 
 import javax.validation.constraints.NotNull;
 import java.util.UUID;
 
 @Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class WorkspaceDTO {
     @NotNull
     private String name;
